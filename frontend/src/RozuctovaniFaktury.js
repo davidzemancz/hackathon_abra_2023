c54fbc25f43276d9d587e362d073b06b3edbc519
kwimbik <kwimbiks@gmail.com>
2023-04-12 14:20:40 +0200

    rozuctovani faktury

--- frontend/src/RozuctovaniFaktury.js
+++ frontend/src/RozuctovaniFaktury.js
@@ -1,4 +1,4 @@
-import { Box, Paper, TextField, Typography } from "@mui/material";
+import { Box, ClickAwayListener, Paper, TextField, Typography } from "@mui/material";
 import ZobrazQueryParametry from "./ZobrazQueryParametry";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -12,6 +12,7 @@ import { GridPravidel } from "./DetailPravidla"
 
 function RozuctovaniFaktury() {
 
+    
     const [faktura, setFaktura] = useState(null);
     const [searchParams, setSearchParams] = useSearchParams()
 
@@ -25,8 +26,12 @@ function RozuctovaniFaktury() {
             const fak = response.data["winstrom"]["faktura-prijata"][0];
             setFaktura(fak);
             console.log("Faktura:", fak);
+            var a = zauctovani_faktury(fak, "", ["A","B","C"]);
+            console.log(a)
         });
     }, []);
+   
+
 
     if (faktura === null) return (<p>Načítání...</p>);
     else return (
@@ -75,7 +80,44 @@ function RozuctovaniFaktury() {
                 </Paper>
             </Grid>
         </Grid>
+
     )
+
+function zauctovani_faktury(faktura, pravidlo) {
+    
+    var MockPravidlo = 
+        { id: 1, Jméno: 'Prvni Pravidlo', View: 'Jon', Četnost: 35, Strediska:  [
+            {klic: "A", hotovost:15, procenta:0, zbytek:true},
+            {klic: "B", hotovost:30, procenta:0, zbytek:false},
+            {klic: "C",hotovost:15, procenta:25, zbytek:false}]
+        }
+
+    var zauctovani = {}
+    var celkovePenez = faktura.sumCelkem;
+    for (let i = 0; i < MockPravidlo.Strediska.length; i++) {
+            zauctovani[MockPravidlo.Strediska[i].klic] = MockPravidlo.Strediska[i].hotovost;
+            celkovePenez -= MockPravidlo.Strediska[i].hotovost;
+            console.log(celkovePenez)   
+    }
+
+    var odecist = 0
+    for (let i = 0; i < MockPravidlo.Strediska.length; i++) {
+        zauctovani[MockPravidlo.Strediska[i].klic] += celkovePenez/100  * MockPravidlo.Strediska[i].procenta;
+        celkovePenez -= celkovePenez/100  * MockPravidlo.Strediska[i].procenta;
+}
+    
+    celkovePenez -= odecist;
+    if (celkovePenez > 0) {
+        for (let i = 0; i < MockPravidlo.Strediska; i++) {
+            if (MockPravidlo.Strediska[i].zbytek == true) {
+                zauctovani[MockPravidlo.Strediska[i].klic] += celkovePenez
+            }
+    }
+    }
+
+    return zauctovani;
+}
+
 }
 
 
